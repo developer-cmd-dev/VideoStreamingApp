e64af5b9cc336671a8065ac76fa192d521e2c5cf
Dev Kumar <devkmandal0@gmail.com>
2025-06-28 18:42:10 +0530

    Changed request path into the signup

--- src/main/java/com/example/Amazon_backend/Controller/UserController.java
+++ src/main/java/com/example/Amazon_backend/Controller/UserController.java
@@ -17,36 +17,18 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/auth")
 public class UserController {
     @Autowired
     private UserService userService;
 
 
-    @PostMapping("signup")
-    public ResponseEntity<List<UserEntity>> saveUser(@RequestBody List<UserEntity> data){
-        List<UserEntity> response = userService.saveUser(data);
+    @PostMapping("/signup")
+    public ResponseEntity<UserDTO> saveUser(@RequestBody UserEntity data){
+        UserDTO response = userService.saveUser(data);
         return ResponseEntity.ok(response);
     }
 
-
-    @GetMapping("get_user/{id}")
-    public ResponseEntity<UserDTO> getUserById(@PathVariable Long id){
-        UserEntity response =userService.getUserById(id).orElse(null);
-        assert response != null;
-        return ResponseEntity.ok(response.toDTO());
-    }
-
-
-    @GetMapping("get_user_by_name/{name}")
-    public ResponseEntity<FindUserDto> getUserByName(@PathVariable String name){
-        Pageable pageRequest = PageRequest.of(0, 5);
-       FindUserDto response = userService.getUserByName(name,pageRequest);
-
-       return new ResponseEntity<>(response, HttpStatus.OK);
-
-
-    }
 
 
 
